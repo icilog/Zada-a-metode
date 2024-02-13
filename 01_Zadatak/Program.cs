a3287d37fe81ab41b6aa2cf962cf4953f16a2d57
icilo <icilo@IVANA>
2024-02-13 09:51:17 +0100

    Add project files.

---
+++ 01_Zadatak/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite rečenicu:");
+            string input = Console.ReadLine();
+            string[] rijeci = input.Split(' ');
+            List<string> listaRijeci = new List<string>();
+            foreach (string rijec in rijeci)
+            {
+                listaRijeci.Add(rijec);
+            }
+
+            Console.WriteLine("Obrnuti ispis unesenoga je {0}", Okreni(listaRijeci));
+
+            Console.ReadKey();
+
+        }
+        static string Okreni(List<string> a)
+        {
+            string okrenuto = "";
+           foreach (string b in a)
+            {
+                for (int i = 0; i < b.Length; i++)
+                {
+                    okrenuto = b[i] + okrenuto;
+                }
+            }
+           return okrenuto;
+        }
+    }
+}
---
+++ 02_Zadatak/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite listu brojeva, razdvoji ih zarezom:");
+            string brojevi = Console.ReadLine();
+            string[] input = brojevi.Split(',');
+            List<int> listaRijeci = new List<int>();
+
+            foreach (var broj in input)
+            {
+                listaRijeci.Add(int.Parse(broj));
+            }
+          
+            List<int> novaLista = Okreni(listaRijeci);
+
+            Console.WriteLine("Obrnuti ispis unesenoga je:");
+            foreach (var nova in novaLista)
+            { Console.Write(nova.ToString());}
+
+            Console.ReadKey();
+
+        }
+        static List<int> Okreni(List<int> a)
+        {
+            a.Reverse();
+            return a;
+        }
+    }
+}
